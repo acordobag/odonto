1d9abd9eb4bea57682719ed793a157d14425aadb
Adrian Cordoba <amcordobag@gmail.com>
2019-08-11 23:33:27 -0600

    test

--- models/models.py
+++ models/models.py
@@ -5,4 +5,4 @@ from odoo import models, fields, api
 class Customer(models.Model):
     _inherit = 'res.partner'
 
-    person_id = fields.Char(string="Cédula", required=True)
+    person_id = fields.Char(string="Cédula")
