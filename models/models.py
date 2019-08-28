34665f29386c85bd2d4238393c4252e35b028504
Adrian Cordoba <amcordobag@gmail.com>
2019-08-27 22:11:43 -0600

    fix

--- models/models.py
+++ models/models.py
@@ -3,14 +3,14 @@
 from odoo import models, fields, api
 
 
-class Customer(models.Model):
-    _inherit = 'res.partner'
-
-    person_id = fields.Char(string="Cédula")
-    job_id = fields.Many2one('odonto.job', string="Job")
-
-
 class Job(models.Model):
     _name = 'odonto.job'
 
     name = fields.Char('Nombre del puesto')
+
+
+class Customer(models.Model):
+    _inherit = 'res.partner'
+
+    person_id = fields.Char(string="Cédula")
+    job_id = fields.Many2one('odonto.job', string="Puesto")
