f1442aef8c3b2d96ef1d5f9558a56a11d3245f5c
Adrian Cordoba <amcordobag@gmail.com>
2019-08-27 23:13:54 -0600

    test

--- models/models.py
+++ models/models.py
@@ -3,14 +3,17 @@
 from odoo import models, fields, api
 
 
-class Job(models.Model):
-    _name = 'odonto.job'
+# class Job(models.Model):
+#     _name = 'odonto.job'
 
-    name = fields.Char('Nombre del puesto')
+#     name = fields.Char('Nombre del puesto')
 
 
 class Customer(models.Model):
     _inherit = 'res.partner'
 
     person_id = fields.Char(string="Cédula")
-    job_id = fields.Many2one('odonto.job', string="Puesto")
+    work_place = fields.Char(string="Lugar de trabajo")
+    birthdate = fields.Date(string="Fecha de nacimiento")
+    marital_status = fields.Selection([(1,'Soltero'),(2,'Casado(a)'),(3,'Divorciado(a)'), (2,'Viudo(a)')],'Estado civil')
+    gender = fields.Selection([(1,'Masculino'),(2,'Femenino')],'Género')
