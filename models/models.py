f942cce869472a423d10f2394d6872b13081e042
Adrian Cordoba <amcordobag@gmail.com>
2019-08-27 21:54:04 -0600

    Relation test

--- models/models.py
+++ models/models.py
@@ -2,7 +2,15 @@
 
 from odoo import models, fields, api
 
+
 class Customer(models.Model):
     _inherit = 'res.partner'
 
     person_id = fields.Char(string="Cédula")
+    job_id = fields.Many2one('odonto.job', string="Job")
+
+
+class Job(models.Model):
+    _name = 'odonto.job'
+
+    name = fields.Char('Nombre del puesto')
