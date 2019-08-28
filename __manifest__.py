7b29a928f3f3ec93de432055e58867a4d8838610
Adrian Cordoba <amcordobag@gmail.com>
2019-08-27 23:37:44 -0600

    doctry

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml'
+        'views/odonto.customer.xml',
+        'views/odonto.event.xml'
     ]
     # only loaded in demonstration mode
     # 'demo': [
--- models/models.py
+++ models/models.py
@@ -3,17 +3,25 @@
 from odoo import models, fields, api
 
 
-# class Job(models.Model):
-#     _name = 'odonto.job'
-
-#     name = fields.Char('Nombre del puesto')
-
-
 class Customer(models.Model):
     _inherit = 'res.partner'
 
     person_id = fields.Char(string="Cédula")
     work_place = fields.Char(string="Lugar de trabajo")
     birthdate = fields.Date(string="Fecha de nacimiento")
-    marital_status = fields.Selection([(1,'Soltero'),(2,'Casado(a)'),(3,'Divorciado(a)'), (2,'Viudo(a)')],'Estado civil')
-    gender = fields.Selection([(1,'Masculino'),(2,'Femenino')],'Género')
+    marital_status = fields.Selection(
+        [(1, 'Soltero'), (2, 'Casado(a)'), (3, 'Divorciado(a)'), (2, 'Viudo(a)')], 'Estado civil')
+    gender = fields.Selection([(1, 'Masculino'), (2, 'Femenino')], 'Género')
+
+
+class doctor(models.Model):
+    _inherit = 'res.users'
+
+    is_doctor = fields.Boolean('Es doctor?')
+    citas = fields.One2many('cxc.route','driver_id','Rutas')
+
+
+class driver(models.Model):
+    _inherit = 'calendar.event'
+
+    user_id = fields.Many2one('res.users', 'Doctor')
