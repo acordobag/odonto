553ac87264fab3cb7b75f6e35f319af279c207e9
Adrian Cordoba <amcordobag@gmail.com>
2019-08-27 22:38:19 -0600

    a

--- models/models.py
+++ models/models.py
@@ -7,11 +7,11 @@ class Job(models.Model):
     _name = 'odonto.job'
 
     name = fields.Char('Nombre del puesto')
-    # people = fields.One2many('res.partner','job_id', 'Gente')
+    people = fields.One2many('res.partner','job_id', 'x')
 
 
 class Customer(models.Model):
     _inherit = 'res.partner'
 
     person_id = fields.Char(string="Cédula")
-    # job_id = fields.Many2one('odonto.job', string="Puesto")
+    job_id = fields.Many2one('odonto.job', string="Puesto")
