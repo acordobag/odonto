81e383a0e43940987fefe396b1f2d95abc3a28ac
Adrian Cordoba <amcordobag@gmail.com>
2019-08-28 22:33:41 -0600

    Fix

--- models/models.py
+++ models/models.py
@@ -24,4 +24,4 @@ class doctor(models.Model):
 class driver(models.Model):
     _inherit = 'calendar.event'
 
-    user_id = fields.Many2one('res.users', 'Doctor')
+    user_id = fields.Many2one('Médico responsable')
